653daafd854d2b63f60f2b9adef20cad90ea3cfc
Murilo Cunha <mcscunha@yahoo.com.br>
2019-10-31 11:44:07 -0300

    Inicio do projeto

---
+++ circula_setor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+Software:
+    CIRCULA_SETOR
+Objetivo:
+    Sistema de troca de comunicados entre setores de uma empresa.
+    Pode ser usado para divulgacao de CIRCULARES, AVISOS, NOTIFICACOES
+        e outras informacoes que se deseja passar na empresa.
+    Semelhante a um chat na rede LAN, no entanto, as informacoes devem 
+        ser mais formais, pois sao gravadas em banco e podem ser usadas
+        como prova material de alguma infracao.
+Linguagem:
+    Python 3.7 (Pacote Anaconda3)
+    Flask (Framework Python para Web)
+    Bootstrap 4 (HTML + CSS + JQuery + JavaScript)
+    Jinja
+    GIT
+    SQLite3 (banco de dados)
+Ambiente:
+    Web (navegadores Firefox e Chrome)
+    Linux (Ubuntu e derivados)
+    Windows (7 e 10)
+Autor:
+    MuriloCunha - mcscunha@yahoo.com.br - 31/10/2019
+
+------------------------------------------------------------------------------
+LOG DE ALTERACOES
+------------------------------------------------------------------------------
+31/10/2019 - Inicio do sistema - Criacao deste arquivo
+
+------------------------------------------------------------------------------
+'''
+
+
+versao = 1.0
+
