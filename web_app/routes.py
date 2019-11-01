addea47a0f2b068b51119d25af2510adcfe2780b
Murilo Cunha <mcscunha@yahoo.com.br>
2019-11-01 15:23:21 -0300

    Continua mais tarde...

--- web_app/routes.py
+++ web_app/routes.py
@@ -1,6 +1,8 @@
 from web_app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'MuriloCunha'}
+    return render_template('index.html', title='Sistema de Comunicação Interna', user=user)
